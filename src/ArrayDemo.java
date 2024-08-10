19e6663dd5cd62fec9f17ee66d3ac6b97b03bfe8
Vaibhav Diwan <divaibhav@gmail.com>
2024-08-10 19:34:16 +0530

    code

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] marks = {10, 20, 30, 40, 50, 60};
+        /*marks[3] = 40;
+        marks[4] = 50;
+        */
+        // INITIALIZING array with for loop
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+
+       /* System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);*/
+        //System.out.println(marks[5]);
+        //System.out.println(marks[6]);
+
+
+    }
 }
